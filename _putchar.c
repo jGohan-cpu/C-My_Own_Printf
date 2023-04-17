7ac7c5bf2c582e605e9ecdcfda67f6123e35b056
jGohan-cpu <jjoseriveram01@gmail.com>
2023-04-17 18:05:07 +0000

    fixing putchar and adding a.out

--- _putchar.c
+++ _putchar.c
@@ -1,14 +1,13 @@
 #include "main.h"
+#include <unistd.h>
 
 /**
- * print_char - print only a single letter
- * @a: character received
- * Return: 1
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-
-int print_char(char a)
+int _putchar(char c)
 {
-	_putchar(a);
-
-	return (1);
-}i
+	return (write(1, &c, 1));
+}
